7e1112b400b94120786f9f026598589a97c92618
Omar Gabriel <omareegab@gmail.com>
2026-08-11 21:04:33 -0500

    conf del proyecto base

--- rag-systems/gnbt/angular-graphql-crud/src/app/main.ts
+++
@@ -1,6 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app.config';
-import { AppComponent } from './app.component'; // ¡Asegúrate de que apunte a nuestro archivo!
-
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
